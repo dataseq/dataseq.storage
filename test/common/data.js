bcfa442840075497982ac248293be3a8f49c6bd8
Dumitru Cantea <canteadumitru@gmail.com>
2015-10-31 18:33:05 +0200

    initing tests

---
+++ test/common/data.js
@@ -0,0 +1,23 @@
+'use strict';
+
+if (!process.env.AWS_REGION) {
+	module.exports = false;
+	console.log('To run all tests you must set ENV variables for AWS');
+	return;
+}
+
+var utils = require('../../lib/utils');
+var Promise = utils.Promise;
+var storage = require('../../lib');
+var ControlService = storage.ControlService;
+var AccessService = storage.AccessService;
+var config = storage.config;
+
+var data = {
+	accessService: new AccessService(),
+	controlService: new ControlService(),
+	createTables: storage.createTables,
+	deleteTables: storage.deleteTables
+};
+
+module.exports = data;
---
+++ test/control_service.js
@@ -0,0 +1,168 @@
+'use strict';
+
+var assert = require('assert');
+var utils = require('../lib/utils');
+var Promise = utils.Promise;
+var Data = require('./common/data');
+var OWNER_ID = 'user_id';
+var SET_ID = 'asdfgh0';
+
+if (!Data) {
+	return;
+}
+
+function foo() {}
+
+describe('ControlService', function() {
+	this.timeout(1000 * 60);
+
+	before('createTables', function() {
+		return Data.createTables();
+	});
+
+	after('deleteTables', function() {
+		return Data.deleteTables('iam-sure');
+	});
+
+	var controlService = Data.controlService;
+	var accessService = Data.accessService;
+
+	describe('#createSet()', function() {
+		it('should create a new Set without id', function() {
+			return controlService.createSet({
+					ownerId: OWNER_ID,
+					name: 'Name 1'
+				})
+				.then(function(set) {
+					assert.ok(set);
+					assert.ok(set.id);
+					assert.equal(OWNER_ID, set.ownerId);
+				});
+		});
+
+		it('should create a new Set with id', function() {
+			return controlService.createSet({
+					id: SET_ID,
+					ownerId: OWNER_ID,
+					name: 'Name 1'
+				})
+				.then(function(set) {
+					assert.ok(set);
+					assert.equal(SET_ID, set.id);
+					assert.equal(OWNER_ID, set.ownerId);
+				});
+		});
+
+		it('should throw error: wrong id', function() {
+			return controlService.createSet({
+					id: 1,
+					ownerId: OWNER_ID,
+					name: 'Name 2'
+				})
+				.catch(function(error) {
+					assert.ok(error);
+					assert.equal(true, !!error.message.indexOf('"id"'));
+				})
+				.then(function(set) {
+					assert.equal(undefined, set);
+				});
+		});
+
+		it('should throw error: no name', function() {
+			return controlService.createSet({
+					ownerId: OWNER_ID
+				})
+				.catch(function(error) {
+					assert.ok(error);
+					assert.equal(true, !!error.message.indexOf('"name"'));
+				})
+				.then(function(set) {
+					assert.equal(undefined, set);
+				});
+		});
+	});
+
+	describe('#findSet()', function() {
+		it('should find one Set', function() {
+			return controlService.createSet({
+					ownerId: OWNER_ID,
+					name: 'Name 1'
+				})
+				.then(function(set) {
+					assert.ok(set);
+					assert.ok(set.id);
+					return accessService.findSet({
+							id: set.id
+						})
+						.then(function(foundedSet) {
+							assert.ok(foundedSet);
+							assert.equal(set.id, foundedSet.id);
+						});
+				});
+		});
+	});
+
+	describe('#createSequence()', function() {
+		it('should create a Sequence', function() {
+			return controlService.createSequence({
+					setId: SET_ID,
+					id: 'us',
+					ownerId: OWNER_ID
+				})
+				.then(function(sequence) {
+					assert.ok(sequence);
+					assert.equal('us', sequence.id);
+					assert.equal(SET_ID, sequence.setId);
+				});
+		});
+		it('should create second Sequence', function() {
+			return controlService.createSequence({
+					setId: SET_ID,
+					id: 'ro',
+					ownerId: OWNER_ID
+				})
+				.then(function(sequence) {
+					assert.ok(sequence);
+					assert.equal('ro', sequence.id);
+					assert.equal(SET_ID, sequence.setId);
+				});
+		});
+		it('should fail on create Sequence', function() {
+			return controlService.createSequence({
+					setId: SET_ID,
+					id: 'ro',
+					ownerId: OWNER_ID
+				})
+				.catch(function(error) {
+					assert.ok(error);
+					assert.equal('ConditionalCheckFailedException', error.code);
+				})
+				.then(function(sequence) {
+					assert.equal(undefined, sequence);
+				});
+		});
+	});
+
+	describe('#findSequence()', function() {
+		it('should find one Sequence', function() {
+			return controlService.createSequence({
+					setId: SET_ID,
+					id: 'ru',
+					ownerId: OWNER_ID
+				})
+				.then(function(sequence) {
+					assert.ok(sequence);
+					assert.equal('ru', sequence.id);
+					assert.equal(SET_ID, sequence.setId);
+					return accessService.findSequence({
+							id: sequence.id,
+							setId: sequence.setId
+						})
+						.then(function(foundedSequence) {
+							assert.ok(foundedSequence);
+							assert.equal(sequence.id, foundedSequence.id);
+						});
+				});
+		});
+	});
+});
